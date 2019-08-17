dda0423d1794902da8b58d88a6ddca9c463eb5cf
motors nsk <motors@live.ru>
2019-08-17 12:27:45 +0700

    clean

--- src/app.ts
+++ src/app.ts
@@ -108,12 +108,4 @@ app.get('/logout', userController.logout)
 
 app.post('/register',userController.validate(create),userController.postRegister)
 
-/**
- * OAuth authentication routes. (Sign in)
- */
-app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email', 'public_profile']}))
-app.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/login'}), (req, res) => {
-    res.redirect(req.session.returnTo || '/')
-})
-
 export default app
