8a07ff003ee8d028d2fd7eaaa41af6eac68ef45a
motors nsk <motors@live.ru>
2019-09-04 23:12:03 +0700

    disable saving of Uninitialized sessions

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import lusca from 'lusca'
 
 import mongo from 'connect-mongo'
 import flash from 'express-flash'
-import path from 'path'
 import mongoose from 'mongoose'
 import passport from 'passport'
 import bluebird from 'bluebird'
@@ -43,14 +42,13 @@ mongoose.connect(mongoUrl, {useNewUrlParser: true}).then(
 
 // Express configuration
 app.set('port', process.env.PORT || 3000)
-app.set('views', path.join(__dirname, '../views'))
-app.set('view engine', 'pug')
+
 app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(session({
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
     secret: SESSION_SECRET,
     store: new MongoStore({
         url: mongoUrl,
@@ -66,24 +64,6 @@ app.use((req, res, next) => {
     res.locals.user = req.user
     next()
 })
-app.use((req, res, next) => {
-    // After successful login, redirect back to the intended page
-    if (!req.user &&
-        req.path !== '/login' &&
-        req.path !== '/signup' &&
-        !req.path.match(/^\/auth/) &&
-        !req.path.match(/\./)) {
-        req.session.returnTo = req.path
-    } else if (req.user &&
-        req.path == '/account') {
-        req.session.returnTo = req.path
-    }
-    next()
-})
-
-app.use(
-    express.static(path.join(__dirname, 'public'), {maxAge: 31557600000})
-)
 
 app.use((req, res, next) => {
     let origin = req.get('origin')
